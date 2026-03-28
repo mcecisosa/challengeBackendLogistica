58261e3cd3735ec18473052fae4cd478031b5e33
cecilia sosa <csosa@elevation.com.ar>
2026-03-28 17:42:37 -0300

    add truck module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { TrucksModule } from './trucks/trucks.module';
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { AuthModule } from './auth/auth.module';
     DatabaseModule,
     UsersModule,
     AuthModule,
+    TrucksModule,
   ],
   controllers: [AppController],
   providers: [AppService],
