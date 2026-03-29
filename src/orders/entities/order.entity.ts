a8d5feb56345e9a9f0f767477ad65b48800a091d
cecilia sosa <csosa@elevation.com.ar>
2026-03-29 19:00:04 -0300

    add OrderStatus as type to status in Order

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,11 +1,12 @@
 import { Location } from 'src/locations/entities/location.entity';
 import { Truck } from 'src/trucks/entities/truck.entity';
 import { User } from 'src/users/entities/user.entity';
+import { OrderStatus } from '../types/order-status.enum';
 
 export class Order {
   constructor(
     public readonly id: string,
-    public status: string,
+    public status: OrderStatus,
     public user: User,
     public truck: Truck,
     public pickup: Location,
